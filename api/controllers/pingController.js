623d91ab21ad283056752dc27c4448dfed9c28b4
dcassinotti <damiancassinotti.33@gmail.com>
2018-09-13 00:51:57 -0300

    Fin de refactor en ping

--- api/controllers/pingController.js
+++ api/controllers/pingController.js
@@ -1,17 +1,11 @@
 'use strict'
 
-var ajaxUtils = require('../utils/ajaxUtils.js'),
+var requestUtils = require('../utils/requestUtils.js'),
 	rp = require('request-promise');
 
 exports.ping = function(req, res) {
-	var options = {
-		url: 'http://app-server-taller2.herokuapp.com/ping',
-		headers: {
-	        'User-Agent': 'Request-Promise'
-	    },
-		json: true
+	var callback = function(data) {
+		res.status(200).json(data);
 	}
-	rp(options)
-		.then(data => res.status(200).json(ajaxUtils.createSimpleResponseOk(data)))
-		.catch(err => res.status(200).json(ajaxUtils.createSimpleResponseError(err)));
+	requestUtils.makeSimpleRequest('http://app-server-taller2.herokuapp.com/ping', callback)
 }
