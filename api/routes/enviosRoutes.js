6b08785908f1e6c84e810bfd635872a2d6db231b
dcassinotti <damiancassinotti.33@gmail.com>
2018-09-18 18:22:44 -0300

    Devuelvo el routing viejo para retrocompatibilidad

--- api/routes/enviosRoutes.js
+++ api/routes/enviosRoutes.js
@@ -2,6 +2,16 @@
 module.exports = function(app, client) {
 	var tracking = require('../controllers/enviosController');
 
+	app.route('/envios')
+		.get(tracking.obtener_envio)
+		.put(tracking.actualizar_envio)
+		.delete(tracking.eliminar_envio)
+
+	app.route('/envios/:envioId')
+		.get(tracking.obtener_envio)
+		.put(tracking.actualizar_envio)
+		.delete(tracking.eliminar_envio)
+
 	app.route('/api/tracking')
 		.get(tracking.obtener_envio)
 		.put(tracking.actualizar_envio)
