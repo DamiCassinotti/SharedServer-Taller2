61c825581a7af55f569c167d6732f1e650ef2c87
dcassinotti <damiancassinotti.33@gmail.com>
2018-10-11 16:54:33 -0300

    Alta de App Servers

---
+++ api/routes/appServerRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const appServerController = require('../controllers/appServerController');
+const errorModel = require('../models/error');
+const appServerValidator = require('../validators/appServerValidator');
+
+const router = express.Router();
+
+router.post('/', (req, res, then) => {
+	var server = req.body;
+	if (!appServerValidator.isValidServerForAdding(server))
+		return res.status(400).json(errorModel.newError(1, 'Parametros erroneos'));
+	appServerController.addServer(server)
+		.then(server => res.status(201).json(server))
+		.catch(error => res.status(500).json(errorModel.newError(0, error.message)));
+});
+
+module.exports = router;
---
+++ api/services/appServerService.js
@@ -0,0 +1,11 @@
+exports.addServer = (server) => {
+	return new Promise((resolve, reject) => {
+		var query = {
+			text: 'insert into server (createdBy, name) values ($1, $2) returning id, _rev, createdBy, createdTime, name, lastConnection',
+			values: [server.createdBy, server.name]
+		}
+		client.query(query)
+			.then(data => resolve(data.rows[0]))
+			.catch(error => reject(error));
+	})
+}
