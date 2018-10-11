7ebacac3d96b6f1297a6ffd4e7b59a0613517e39
dcassinotti <damiancassinotti.33@gmail.com>
2018-10-11 00:23:00 -0300

    Creacion de route

--- api/routes/paymentsRoutes.js
+++ api/routes/paymentsRoutes.js
@@ -5,6 +5,12 @@ const paymentValidator = require('../validators/paymentValidator');
 
 const router = express.Router();
 
+router.get('/', (req, res, then) => {
+	paymentsController.getPayments()
+		.then(payments => res.status(200).json(payments))
+		.catch(error => res.status(500).json(errorModel.newError(0, error.message)));
+})
+
 router.post('/', (req, res, then) => {
 	var payment = req.body;
 	if (!paymentValidator.isValidPayment(payment))
