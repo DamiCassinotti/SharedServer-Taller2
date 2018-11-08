79f2ee5aea45865dda1b190efc5d3e3b0e01c079
dcassinotti <damiancassinotti.33@gmail.com>
2018-11-07 22:04:13 -0300

    Fix Merge

--- db/scripts/login.sql
+++ db/scripts/login.sql
@@ -3,4 +3,4 @@ create table if not exists usuario (
 	password text not null
 );
 
-insert into usuario ("username", "password") values ('administrator', 'password'), ('pgotuzzo', 'password'), ('dcassinotti', 'password')
+insert into usuario ("username", "password") values ('administrator', 'password'), ('pgotuzzo', 'password'), ('dcassinotti', 'password');
