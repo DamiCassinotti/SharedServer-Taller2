8d34d3b53e581ffd72366f42da24540274745a3c
dcassinotti <damiancassinotti.33@gmail.com>
2018-11-07 23:03:10 -0300

    Hot-Fix: Filtro OPTIONS

--- server.js
+++ server.js
@@ -17,10 +17,13 @@ bootstrapApp = () => {
 	var app = express();
 
 	app.use(function(req, res, next) {
-	  res.header('Access-Control-Allow-Origin', '*');
-	  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-	  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-	  next();
+		res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+		res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+		if (req.method == 'OPTIONS')
+	  		res.send(200)
+		else
+			next();
 	});
 
 	app.use(jwt({secret: config.tokens.secret}).unless({
