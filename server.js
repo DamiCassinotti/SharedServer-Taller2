00ae032586226219612b4e00af220b7cba133ab1
dcassinotti <damiancassinotti.33@gmail.com>
2018-11-07 22:57:50 -0300

    Hot-Fix: cors header

--- server.js
+++ server.js
@@ -17,8 +17,9 @@ bootstrapApp = () => {
 	var app = express();
 
 	app.use(function(req, res, next) {
-	  res.header("Access-Control-Allow-Origin", "*");
-	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+	  res.header('Access-Control-Allow-Origin', '*');
+	  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
 	  next();
 	});
 
