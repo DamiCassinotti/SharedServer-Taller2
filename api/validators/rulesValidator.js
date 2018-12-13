862c3f35ab479a6f4fabdd8d1346ce943507c490
dcassinotti <damiancassinotti.33@gmail.com>
2018-12-13 01:49:15 -0300

    Fix en validacion de reglas

--- api/validators/rulesValidator.js
+++ api/validators/rulesValidator.js
@@ -1,5 +1,5 @@
 exports.isValidRule = (rule) => {
-	if (!rule || !rule.description || !rule.description || !rule.condition || !rule.type || !rule.value || (rule.type == 'multiply' && !rule.property))
+	if (!rule || !rule.description || !rule.description || !rule.condition || !rule.type || isNaN(rule.value) || (rule.type == 'multiply' && !rule.property))
 		return false;
 	return true;
 };
